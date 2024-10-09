55114184394d2af0b0b63b13afcdb4b651476295
Jonesey86 <irj082024@gmail.com>
2024-10-09 14:44:18 +0200

    mvc second try initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿﻿namespace KH_MVC;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Model model = new Model("Ian Jones", 38);
+        View view = new View();
+
+        Controller controller = new Controller(model, view);
+
+        // Show the model
+        controller.ShowModel();
+
+        // update the name & age
+        controller.UpdateAge();
+
+
+        // display the updated model
+        controller.ShowModel();
+
+        // Keep the conole open for input
+        Console.ReadLine();
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
